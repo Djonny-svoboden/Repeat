ac29df9f1b8fb0092b9e4c475b8105c5da5ca671
Djonni-svoboden <djinnic-forever15@gmail.com>
2019-11-28 22:50:28 +0200

    Функцiя яка ховає таби

---
+++ Homework/main.js
@@ -0,0 +1,7 @@
+function openTag(event, Name) {
+    var i, innerTab, buttab
+    innerTab = document.getElementsByClassName("inner-tab");
+    for (i = 0; i < tabcontent.lenght; i++) {
+        innerTab[i].style.display = "none"
+    }
+}
