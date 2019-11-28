a01e9ea3c1ac1d96b50a8465c5ca20d7bb844ff4
Djonni-svoboden <djinnic-forever15@gmail.com>
2019-11-28 23:39:36 +0200

    Функцiя яка ховає таби та змiнює стиль

--- Homework/main.js
+++ Homework/main.js
@@ -5,3 +5,11 @@ window.onload = function() {
     bultab = document.getElementsByClassName('buttab')
     hidetabs(1);
 }
+
+function hidetabs(a) {
+    for (var i = a; i < tabCont.length; i++) {
+        tabCont[i].classList.remove('show');
+        tabCont[i].classList.add("hide");
+        bultab[i].classList.remove('fullborder');
+    }
+}
